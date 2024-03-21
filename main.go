43a29c146b4e87d7fa67923f88f2d577ad98d2d8
BayuFA <bayufa@gmail.com>
2024-03-21 09:24:10 +0700

    flags, load file

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"bufio"
 	"context"
+	"encoding/csv"
+	"flag"
 	"fmt"
 	"os"
 	"strings"
@@ -17,11 +19,23 @@ import (
 type MyLogger struct{}
 
 func (l MyLogger) InfoF(msg string, params ...interface{}) {
-	log.Info().Msgf(msg, params...)
+	if debug {
+		log.Info().Msgf(msg, params...)
+	}
 }
 func (l MyLogger) ErrorF(msg string, params ...interface{}) {
 	log.Error().Msgf(msg, params...)
 }
+
+var (
+	serverAddr string
+	systemID   string
+	password   string
+	systemType string
+	timeout    int
+	smsFile    string
+	debug      bool
+)
 
 func main() {
 	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr, TimeFormat: time.TimeOnly})
@@ -29,18 +43,28 @@ func main() {
 
 	log.Info().Msgf("smpp-client-load-tester")
 
+	flag.StringVar(&serverAddr, "addr", "127.0.0.1:2775", "SMSC server address and port")
+	flag.StringVar(&systemID, "systemid", "esme", "Identifies the ESME system")
+	flag.StringVar(&password, "password", "password", "The password may be used by the SMSC to authenticate the ESME")
+	flag.StringVar(&systemType, "systemtype", "smpp", "Identifies the type of ESME system")
+	flag.IntVar(&timeout, "timeout", 10, "Smpp request timeout")
+	flag.StringVar(&smsFile, "smsfile", "sms.csv", "Import sms data from this file")
+	flag.BoolVar(&debug, "smppdebug", true, "Show smpp debug log")
+	flag.Parse()
+
 	msgId := 1
 	bindConf := smpp.BindConf{
-		SystemID:   "esme1",
-		Password:   "password",
-		SystemType: "smpp",
-		Addr:       "127.0.0.1:5000",
+		SystemID:   systemID,
+		Password:   password,
+		SystemType: systemType,
+		Addr:       serverAddr,
 	}
 
 	sessionConf := smpp.SessionConf{
-		SendWinSize: 10000,
-		ReqWinSize:  10000,
-		Logger:      MyLogger{},
+		SendWinSize:   10000,
+		ReqWinSize:    10000,
+		WindowTimeout: time.Duration(timeout * int(time.Second)),
+		Logger:        MyLogger{},
 		SessionState: func(sessionID string, systemID string, state smpp.SessionState) {
 		},
 		Handler: smpp.HandlerFunc(func(ctx *smpp.Context) {
@@ -94,39 +118,27 @@ func main() {
 		}),
 	}
 
-	startTaskTimer(1*time.Second, func() bool {
+	startTaskTimer(5*time.Second, func() bool {
 		log.Info().Msgf("Connecting to smpp server...%v", bindConf)
 		session, err := smpp.BindTRx(sessionConf, bindConf)
 		if err != nil {
 			log.Error().Msgf("BindTRX failed error: %+v", err)
-			session.Close()
+			if session != nil {
+				session.Close()
+			}
 			return false
 		}
+		log.Info().Msgf("Connected to smpp server. Preparing sms..")
 
-		time.Sleep(5 * time.Second)
-		start := time.Now()
-		for i := 0; i < 100; i++ {
-			sm := &pdu.SubmitSm{
-				SourceAddr:      "11111111",
-				DestinationAddr: "22222222",
-				ShortMessage:    "Hello from SMPP!",
-			}
-			resp, _, err := session.Send(context.Background(), sm)
-			if err != nil {
-				log.Error().Msgf("SubmitSM failed error: %+v", err)
-			} else {
-				log.Debug().Msgf("Recv response %s %+v", resp.CommandID(), resp)
-			}
-		}
-		elapsed := time.Since(start).Milliseconds()
-		log.Debug().Msgf("Time elapsed: %d ms\n", elapsed)
+		time.Sleep(1 * time.Second)
+		loadAndSendSms(session)
+
 		return true
 	})
 
 	scanner := bufio.NewScanner(os.Stdin)
 	for {
-		fmt.Println("[q] quit")
-		fmt.Print("Command: ")
+		fmt.Print("Press [q] to quit\n\n")
 		scanner.Scan()
 		command := strings.ToLower(scanner.Text())
 		if command == "q" {
@@ -136,6 +148,61 @@ func main() {
 			fmt.Println("Unknown command: " + command)
 		}
 	}
+}
+
+func loadAndSendSms(session *smpp.Session) {
+	start := time.Now()
+
+	log.Info().Msgf("Opening sms file...")
+	file, err := os.Open(smsFile)
+	if err != nil {
+		log.Fatal().Msgf("Error: %s", err)
+		return
+	}
+	defer file.Close()
+
+	reader := csv.NewReader(file)
+	reader.LazyQuotes = true
+	reader.FieldsPerRecord = 3
+	records, err := reader.ReadAll()
+	if err != nil {
+		log.Fatal().Msgf("Read file error : %v", err)
+		return
+	}
+	log.Info().Msgf("Sms file loaded.")
+
+	log.Info().Msgf("Sending %v sms...", len(records))
+	for _, record := range records {
+		sm := &pdu.SubmitSm{
+			SourceAddr:      record[0],
+			DestinationAddr: record[1],
+			ShortMessage:    record[2],
+		}
+		_, _, err := session.Send(context.Background(), sm)
+		if err != nil {
+			log.Error().Msgf("SubmitSM failed: %+v", err)
+		} else {
+			// log.Debug().Msgf("Recv response %s %+v", resp.CommandID(), resp)
+		}
+	}
+	log.Info().Msgf("Sending sms done")
+
+	// for i := 0; i < 100; i++ {
+	// 	sm := &pdu.SubmitSm{
+	// 		SourceAddr:      "11111111",
+	// 		DestinationAddr: "22222222",
+	// 		ShortMessage:    "Hello from SMPP!",
+	// 	}
+	// 	resp, _, err := session.Send(context.Background(), sm)
+	// 	if err != nil {
+	// 		log.Error().Msgf("SubmitSM failed error: %+v", err)
+	// 	} else {
+	// 		log.Debug().Msgf("Recv response %s %+v", resp.CommandID(), resp)
+	// 	}
+	// }
+
+	elapsed := time.Since(start).Milliseconds()
+	log.Info().Msgf("Time elapsed: %d ms\n", elapsed)
 }
 
 func startTaskTimer(interval time.Duration, fn func() bool) chan bool {
@@ -154,6 +221,5 @@ func startTaskTimer(interval time.Duration, fn func() bool) chan bool {
 			}
 		}
 	}()
-
 	return stop
 }
